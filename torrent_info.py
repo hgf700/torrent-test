2f0542a88dc645ae4f44d9eb290dbf8bf044c25e
hgf700 <pawellapinski33@gmail.com>
2025-03-09 21:28:54 +0100

    showing entire pieces

--- torrent_info.py
+++ torrent_info.py
@@ -6,33 +6,39 @@ import hashlib
 def parse_file(file):
     try:
         with open(file, "rb") as f:
-            decoded_data=bencodepy.decode(f.read())
+            decoded_data = bencodepy.decode(f.read())
 
-        tracker_url = decoded_data.get(b'announce', b'').decode() 
-        length = decoded_data.get(b'info', {}).get(b'length', 0) 
-        to_code = decoded_data.get(b'info',{})
+        tracker_url = decoded_data.get(b'announce', b'').decode()
+        length = decoded_data.get(b'info', {}).get(b'length', 0)
+        to_code = decoded_data.get(b'info', {})
         coded = bencodepy.encode(to_code)
-        hash = hashlib.sha1(coded).hexdigest()
+        info_hash = hashlib.sha1(coded).hexdigest()
+        piece_length = decoded_data.get(b'info', {}).get(b'piece length', 0)
+        pieces = decoded_data.get(b'info', {}).get(b'pieces', b'')
+        # show pieces in 20 long parts
+        piece_hashes = [pieces[i:i+20].hex() for i in range(0, len(pieces), 20)]
 
         return {
             "Tracker URL": tracker_url,
             "Length": length,
-            "Coded SHA1" : hash
+            "Info Hash": info_hash,
+            "Piece Length": piece_length,
+            "Piece Hashes": piece_hashes
         }
 
     except Exception as e:
-        print(e)
+        print(f"Błąd: {e}")
         sys.exit(1)
 
 def main():
-    command = sys.argv[1]
+    if len(sys.argv) != 3 or sys.argv[1] != "info":
+        print("Użycie: python main.py info <ścieżka_do_pliku.torrent>")
+        sys.exit(1)
 
-    if command == "info":
-        file_path = sys.argv[2]
+    file_path = sys.argv[2]
     result = parse_file(file_path)
 
     print(json.dumps(result, indent=4))
-    
 
 if __name__ == "__main__":
     main()
