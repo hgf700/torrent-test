0eea47f7bce7393265b443e11e0f738f75db8ba7
hgf700 <pawellapinski33@gmail.com>
2025-03-19 12:11:40 +0100

    feat: attemp with downloading piece of torrent and add extract data from magnet link

---
+++ magnet_files.py
@@ -0,0 +1,35 @@
+import sys
+import re
+import urllib.parse
+import hashlib
+
+def parse_magnet(magnet_link):
+    match = re.search(r"xt=urn:btih:([a-fA-F0-9]+)", magnet_link)
+    if match:
+        info_hash = match.group(1)
+    else:
+        info_hash = "Nie znaleziono"
+    
+    sha1_hash = hashlib.sha1(bytes(info_hash, 'utf-8')).hexdigest()
+
+    match_dn = re.search(r"dn=([^&]+)", magnet_link)
+    file_name = match_dn.group(1) if match_dn else "Nie znaleziono"
+
+    match_tr = re.search(r"tr=([^&]+)", magnet_link)
+    tracker_url = urllib.parse.unquote(match_tr.group(1)) if match_tr else "Nie znaleziono"
+    
+    print(f"Info Hash: {sha1_hash}")
+    print(f"Nazwa pliku: {file_name}")
+    print(f"Tracker URL: {tracker_url}")
+
+    
+def main():
+    if len(sys.argv) < 2:
+        print("Użycie: python magnet_files.py 'magnet:?xt=...'")
+        sys.exit(1)
+
+    magnet_link = sys.argv[1]
+    parse_magnet(magnet_link)
+
+if __name__ == "__main__":
+    main()
